01d80a70c0877fdf16a4e6b40b2f6cb1bdd45a99
DV1712 <37181116+DV1712@users.noreply.github.com>
2020-02-11 15:23:20 +0530

    login page is blank..reg has some css to it and little favicon addition little model created

---
+++ Users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from Users import views
+from Users import templates
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
+urlpatterns = [
+    path("registration/", views.registration_page, name="Reg Page"),
+    path("login/", views.login_page, name="Login Page"),
+    path("", views.home, name="Home Page"),
+]
--- registration/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from registration import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("registration/<name>", views.hello_there, name="hello_there"),
-]
-
